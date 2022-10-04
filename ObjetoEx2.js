46a9df1e6be531fe0c4f98b605a78ea6bcb1f5b5
Rafael Felipe <rafael.giubilei@hotmail.com>
2022-10-04 15:05:25 -0300

    exercicio 2 objetos

---
+++ ObjetoEx2.js
@@ -0,0 +1,23 @@
+const cartao = {
+    nome: "Rafael",
+    idade: 26,
+    produtos: [
+        {
+            nome: "pao de alho",
+            precoUnit: 1500,
+            quantidade: 5
+        },
+        {
+            nome: "cerveja",
+            precoUnit: 1000,
+            quantidade: 2
+        },
+        {
+            nome: "agua",
+            precoUnit: 500,
+            quantidade: 1
+        }
+    ]
+};
+
+console.log(cartao);
