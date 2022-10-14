71e24fe5b43e9c54af0e13471585e35b94b924f1
Rafael Felipe <rafael.giubilei@hotmail.com>
2022-10-14 10:00:57 -0300

    Create Revisão-funcoes.js

---
+++ Funçoes/Revisão-funcoes.js
@@ -0,0 +1,16 @@
+function verificacao (idadeDaPessoa){
+
+    let { nome, idade, peso, profissao } = idadeDaPessoa
+
+    console.log(`meu nome é ${nome}, peso ${peso}, idade ${idade} e sou ${profissao}.`)
+}
+
+let pessoa = {
+    nome: "rafael",
+    idade: 26,
+    altura: 1.71,
+    peso: 68,
+    profissao: "DEV"
+}
+
+verificacao(pessoa);
